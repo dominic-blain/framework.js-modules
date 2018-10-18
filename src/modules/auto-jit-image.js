3266a834f6a92461d6522677a253ad7183509c29
Dominic Blain <dominic.blain@gmail.com>
2018-10-18 16:50:45 -0400

    Add fix to jit bg image size

--- src/modules/auto-jit-image.js
+++ src/modules/auto-jit-image.js
@@ -21,25 +21,79 @@
 		}
 	};
 
+	var updateJitDimension = function (t) {
+		var ctn = t.closest('.jit-image-bg');
+		var ctnRatio = ctn.height() / Math.max(ctn.width(), 1);
+		var ratio = t.attr('data-height') / Math.max(t.attr('data-width'), 1);
+		var srcFormat = t.attr('data-src-format');
+		var dimension = '$w';
+
+		// Landscape or Square
+		if (ctnRatio <= 1) {
+			dimension = ratio < ctnRatio ? '$h' : '$w';
+		// Portrait or else
+		} else {
+			dimension = ratio > ctnRatio ? '$w' : '$h';
+		}
+
+		// Check if dimension needs to change
+		if (srcFormat.indexOf(dimension) == -1) {
+			var newSrcFormat = '';
+			if (dimension == '$w') {
+				newSrcFormat = srcFormat.replace('0/$h', '$w/0');
+			}
+			else if (dimension == '$h') {
+				newSrcFormat = srcFormat.replace('$w/0', '0/$h');
+			}
+			t.attr('data-src-format', newSrcFormat).jitImage();
+		}
+	};
+
+	var processAllBgImages = function () {
+		$(site).find('img[data-src-format].jit-image-bg-src').each(function (i, e) {
+			var t = $(this);
+			updateJitDimension(t);
+		});
+	};
+
+	var onResize = function () {
+		processAllBgImages();
+	};
+
 	var onArticleEnter = function (key, data) {
-		$(data.article).find('img[data-src-format]').jitImage();
+		if (!firstTime) {
+			processAllBgImages();
+			$(data.article).find('img[data-src-format]').jitImage();
+		}
 	};
 
 	var onEnter = function (key, data) {
 		if (!firstTime) {
+			processAllBgImages();
 			$(data.page.key()).find('img[data-src-format]').jitImage();
 		}
 		firstTime = false;
 	};
 
+	var loaded = function () {
+		setTimeout(function () {
+			processAllBgImages();
+			$('#site img[data-src-format]').jitImage();
+		}, 500);
+	};
+
 	var init = function () {
 		site.on('loaded.jitImage', onJitLoaded);
 	};
-	
+
 	var actions = function () {
 		return {
 			page: {
 				enter: onEnter
+			},
+			site: {
+				loaded: loaded,
+				resize: onResize
 			},
 			articleChanger: {
 				enter: onArticleEnter
